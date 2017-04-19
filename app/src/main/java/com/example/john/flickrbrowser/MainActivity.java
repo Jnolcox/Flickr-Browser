12fd318f9f760e1f14aba6b6e110a6529748a129
Jnolcox <Jnolcox0429@gmail.com>
2017-04-19 15:46:59 -0400

    created baseactivity class

--- app/src/main/java/com/example/john/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/example/john/flickrbrowser/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.john.flickrbrowser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -8,7 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private static final String LOG_TAG = "MainActivity";
     private List<Photo> mPhotosList = new ArrayList<Photo>();
@@ -19,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activateToolbar();
+
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
