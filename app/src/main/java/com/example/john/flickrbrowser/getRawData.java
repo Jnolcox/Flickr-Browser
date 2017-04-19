abdf1f53f16b683166f1fb92db1d456ef76fa51c
Jnolcox <Jnolcox0429@gmail.com>
2017-04-19 01:59:22 -0400

    Initial commit + raw data grab

---
+++ app/src/main/java/com/example/john/flickrbrowser/getRawData.java
@@ -0,0 +1,92 @@
+package com.example.john.flickrbrowser;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+enum DownloadStatus { IDLE, PROCESSING, NOT_INITIALISED, FAILED_OR_EMPTY, OK }
+public class getRawData {
+    private String LOG_TAG = getRawData.class.getSimpleName();
+    private String mRawUrl;
+    private String mData;
+    private DownloadStatus mDownloadStatus;
+
+    public getRawData(String mRawUrl) {
+        this.mRawUrl = mRawUrl;
+        this.mDownloadStatus = DownloadStatus.IDLE;
+    }
+
+    public void reset() {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        this.mRawUrl = null;
+        this.mData = null;
+    }
+
+    public String getmData() {
+        return mData;
+    }
+
+    public DownloadStatus getmDownloadStatus() {
+        return mDownloadStatus;
+    }
+
+    public class DownloadRawData extends AsyncTask<String, Void, String> {
+
+        protected void onPostExecute(String webData) {
+            //TODO: Fill in later
+        }
+
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            if (params == null)
+                return null;
+
+            try {
+                URL url = new URL(params[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    return null;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                return buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+        }
+    }
+}
