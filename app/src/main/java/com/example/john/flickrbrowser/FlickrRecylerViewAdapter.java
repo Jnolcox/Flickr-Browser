6b63e5aa3dd5c749872eed3048f3a85b6c2596ac
Jnolcox <Jnolcox0429@gmail.com>
2017-04-19 05:28:51 -0400

    Updated Gradle imported recyclerview started GUI

---
+++ app/src/main/java/com/example/john/flickrbrowser/FlickrRecylerViewAdapter.java
@@ -0,0 +1,7 @@
+package com.example.john.flickrbrowser;
+
+import android.support.v7.widget.RecyclerView;
+
+public class FlickrRecylerViewAdapter extends RecyclerView.Adapter<FlickrImageViewHolder> {
+
+}
